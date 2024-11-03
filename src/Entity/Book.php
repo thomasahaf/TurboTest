c1d5208af85e6b902e37f5081befd5ca220f41c4
Thomas Hafsaas <thomashafsaas@gmail.com>
2024-11-03 17:14:42 +0100

    Updated

---
+++ src/Entity/Book.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\UX\Turbo\Attribute\Broadcast;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[Broadcast]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Author = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): static
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->Author;
+    }
+
+    public function setAuthor(string $Author): static
+    {
+        $this->Author = $Author;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+}
